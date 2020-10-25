fcfdd356c4873dad7f7442f0fbfdb5e6dc0184d4
meiirbek.islamov <mei12@pitt.edu>
2020-10-25 12:02:32 -0400

    Add setup.py

--- setup.py
+++ setup.py
@@ -1,13 +1,13 @@
 #!/usr/bin/env python
 
 from distutils.core import setup
-setup(name='defectMOF',
+setup(name='defect',
       version='0.1.0',
       description='Introducing Missing-linker Defects in Metal-Organic Frameworks',
       author='Meiirbek Islamov',
       author_email='mei12@pitt.edu',
       url='https://github.com/meiirbek-islamov/defects',
-      packages=['defectMOF'],
+      packages=['defect'],
       install_requires=[
           'numpy',
           'click',
